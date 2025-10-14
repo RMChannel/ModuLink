c8fcb6ffda97786780edd3bbe831bf8f3464e0de
Roberto Cito <98057423+RMChannel@users.noreply.github.com>
2025-10-14 12:01:57 +0200

    First build

---
+++ src/test/java/com/oct/modulink/ModuLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oct.modulink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
