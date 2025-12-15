11b69ddd05e807249b5ce47d2e072fc5b63ea18d
RMChannel <rmchannel82@gmail.com>
2025-12-15 16:04:51 +0100

    Classe modulo creata, index diviso in fragment con heeader e footer

---
+++ src/main/java/com/modulink/Model/Modulo/ModuloEntity.java
@@ -0,0 +1,64 @@
+package com.modulink.Model.Modulo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "modulo",schema = "modulink")
+public class ModuloEntity {
+    @Id
+    @Column(name = "id_modulo", nullable = false)
+    private int id_modulo;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "descrizione", nullable = false)
+    private String descrizione;
+
+    @Column(name = "url_icona")
+    private String url_icona;
+
+    public ModuloEntity(){}
+
+    public ModuloEntity(int id_modulo, String nome, String descrizione, String url_icona) {
+        this.id_modulo = id_modulo;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.url_icona = url_icona;
+    }
+
+    public int getId_modulo() {
+        return id_modulo;
+    }
+
+    public void setId_modulo(int id_modulo) {
+        this.id_modulo = id_modulo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getUrl_icona() {
+        return url_icona;
+    }
+
+    public void setUrl_icona(String url_icona) {
+        this.url_icona = url_icona;
+    }
+}
---
+++ src/main/java/com/modulink/Model/Modulo/ModuloRepository.java
@@ -0,0 +1,6 @@
+package com.modulink.Model.Modulo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuloRepository extends JpaRepository<ModuloEntity, Integer> {
+}
