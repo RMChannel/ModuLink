cd414b193fb81990799e9f9ebf0bf4e044b4f204
Arjel Buzi <arjel.buzi1@gmail.com>
2025-12-29 20:16:00 +0100

    prima implementazione di GDU

--- src/main/java/com/modulink/Model/Modulo/ModuloRepository.java
+++ src/main/java/com/modulink/Model/Modulo/ModuloRepository.java
@@ -1,6 +1,16 @@
 package com.modulink.Model.Modulo;
 
+import com.modulink.Model.Utente.UtenteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ModuloRepository extends JpaRepository<ModuloEntity, Integer> {
+
+    @Query("SELECT DISTINCT aff.attivazione.modulo FROM AffiliazioneEntity aff " +
+           "JOIN AssociazioneEntity ass ON aff.ruolo = ass.ruolo " +
+           "WHERE ass.utente = :utente")
+    List<ModuloEntity> findModuliByUtente(@Param("utente") UtenteEntity utente);
 }
