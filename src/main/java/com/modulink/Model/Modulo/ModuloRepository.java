9f105a5cbdd38f18bddc081a4bb146b17c0a75bb
Roberto Cito <rmchannel82@gmail.com>
2026-01-16 16:38:12 +0100

    Affiliazione -> Pertinenza
    Signed-off-by: Roberto Cito <rmchannel82@gmail.com>

--- src/main/java/com/modulink/Model/Modulo/ModuloRepository.java
+++ src/main/java/com/modulink/Model/Modulo/ModuloRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ModuloRepository extends JpaRepository<ModuloEntity, Integer> {
 
-    @Query("SELECT DISTINCT aff.attivazione.modulo FROM AffiliazioneEntity aff " +
+    @Query("SELECT DISTINCT aff.attivazione.modulo FROM PertinenzaEntity aff " +
            "JOIN AssociazioneEntity ass ON aff.ruolo = ass.ruolo " +
            "WHERE ass.utente = :utente")
     List<ModuloEntity> findModuliByUtente(@Param("utente") UtenteEntity utente);
@@ -17,7 +17,7 @@ public interface ModuloRepository extends JpaRepository<ModuloEntity, Integer> {
     @Query("SELECT m FROM ModuloEntity m WHERE :url LIKE CONCAT(m.url_modulo, '%') ORDER BY LENGTH(m.url_modulo) DESC")
     List<ModuloEntity> findMatchingModuli(@Param("url") String url);
 
-    @Query("SELECT DISTINCT aff.attivazione.modulo FROM AffiliazioneEntity aff " +
+    @Query("SELECT DISTINCT aff.attivazione.modulo FROM PertinenzaEntity aff " +
             "JOIN AssociazioneEntity ass ON aff.ruolo = ass.ruolo " +
             "WHERE ass.utente = :utente AND aff.id_modulo= :id")
     List<ModuloEntity> isModuloAccessible(@Param("id") int id, @Param("utente") UtenteEntity utente);
---
+++ src/main/java/com/modulink/Model/Relazioni/Pertinenza/PertinenzaService.java
@@ -0,0 +1,34 @@
+package com.modulink.Model.Relazioni.Pertinenza;
+
+import com.modulink.Model.Azienda.AziendaEntity;
+import com.modulink.Model.Relazioni.Attivazione.AttivazioneEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PertinenzaService {
+    private final PertinenzaRepository pertinenzaRepository;
+
+    public PertinenzaService(PertinenzaRepository pertinenzaRepository) {
+        this.pertinenzaRepository = pertinenzaRepository;
+    }
+
+    @Transactional
+    public void attivazioneDefault(AziendaEntity aziendaEntity) {
+        List<PertinenzaEntity> affiliazioni=new ArrayList<>();
+
+        affiliazioni.add(new PertinenzaEntity(0,0,aziendaEntity.getId_azienda()));
+        affiliazioni.add(new PertinenzaEntity(0,1,aziendaEntity.getId_azienda()));
+        affiliazioni.add(new PertinenzaEntity(0,2,aziendaEntity.getId_azienda()));
+        affiliazioni.add(new PertinenzaEntity(0,3,aziendaEntity.getId_azienda()));
+
+        pertinenzaRepository.saveAll(affiliazioni);
+    }
+
+    public List<PertinenzaEntity> findAllByAttivazione(AttivazioneEntity attivazione) {
+        return pertinenzaRepository.findAllByAttivazione(attivazione);
+    }
+}
